5079f589ae4b2b7abd0622caf2a0839ec73b6cfe
Magali JOUAN <jouan.magali@gmail.com>
2025-11-19 16:43:34 +0100

    modification mune mobile en mode non connecté

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/boardgames';
 
     /**
      * Create a new controller instance.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,52 +29,39 @@
                     <img src="/images/logo-meeplix.png" alt="Meeplix" width="32" height="32" class="me-2">
                     <span class="title">{{ config('app.name', 'Laravel') }}</span>
                 </a>
-                @auth
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                @endauth
 
-                <div class="@auth collapse navbar-collapse @else navbar-collapse @endauth" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
+                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                <li class="nav-item dropdown"><a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <li class="nav-item dropdown"><a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                             @endif
                         @else
+                            @if(auth()->check()) 
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        @if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->hasRole('moderator')))
-                                            <a class="dropdown-item" href="{{ route('dashboard.boardgames.index') }}">Boardgames</a>
-                                        @endif
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
+                                {{ Auth::user()->name }}
+                            </li>
+                            @endif
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
+                            @if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->hasRole('moderator')))
+                                <li class="nav-item dropdown"><a class="dropdown-item" href="{{ route('dashboard.boardgames.index') }}">Boardgames</a></li>
+                            @endif
+                            <li class="nav-item dropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                             </li>
                         @endguest
                     </ul>
