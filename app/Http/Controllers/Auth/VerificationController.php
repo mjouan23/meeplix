de1fbd3eff138d8cf27b1cd0fd7205986aaa8afa
Magali JOUAN <jouan.magali@gmail.com>
2025-11-30 15:05:06 +0100

    Redirection boardgames au lieu de home

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/boardgames';
 
     /**
      * Create a new controller instance.
